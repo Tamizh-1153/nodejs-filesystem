cb43e90f3d2289d9aacb0280973c649860478a7c
Tamizh-1153 <128562854+Tamizh-1153@users.noreply.github.com>
2023-06-06 16:30:08 +0530

    node js filesystem

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express")
+const fs = require("fs")
+const path = require("path")
+const app = express()
+
+const folderPath = path.resolve(__dirname, "Files")
+
+app.post("/create-text-file", (req, res) => {
+    
+    const date = new Date()
+    const filename = `file_${Date.now()}.txt`
+
+  fs.writeFile(
+    `${folderPath}/${filename}`,
+    `File created at ${date}`,
+    (err) => {
+      if (err) {
+        console.error(err)
+        res.status(500).send("An error occurred while creating the file.")
+      } else {
+        res.send("File created successfully!")
+      }
+    }
+  )
+})
+
+app.get("/files", (req, res) => {
+  fs.readdir(folderPath, (err, files) => {
+    if (err) {
+      console.error(err)
+      res.status(500).send("An error occurred while retrieving the files.")
+    } else {
+      res.json(files)
+    }
+  })
+})
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000")
+})
